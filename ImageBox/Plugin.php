5b82af2eb2f45520fcab40d27f39f4c59568e40e
journey-ad <journey.adc@gmail.com>
2016-01-13 15:43:28 +0800

    增加自动加载jQuery功能

--- ImageBox/Plugin.php
+++ ImageBox/Plugin.php
@@ -43,6 +43,10 @@ class ImageBox_Plugin implements Typecho_Plugin_Interface
 	public static function config(Typecho_Widget_Helper_Form $form)
 	{
 	    echo '一个简单的、基于jQuery的图片灯箱插件。<br/>原作者：<a href="https://github.com/kirainmoe/Image-Box" target="_blank">吟梦</a>';
+	    $jquery = new Typecho_Widget_Helper_Form_Element_Radio(
+        'jquery', array('0'=> '手动加载', '1'=> '自动加载'), 0, 'jQuery',
+            '“手动加载”需要你手动加载jQuery，若选择“自动加载”，插件会自动加载jQuery，版本为1.9.1。');
+        $form->addInput($jquery);
 		$Selector = new Typecho_Widget_Helper_Form_Element_Text('Selector', NULL, 'div img', _t('选择器'), _t('(默认div img)请根据你的主题输入适合的值，默认值已经适合大部分情况。<a href="http://www.w3school.com.cn/jquery/jquery_ref_selectors.asp" target="_blank">jQuery选择器说明</a>'));
         $form->addInput($Selector);
 	}
@@ -61,6 +65,9 @@ class ImageBox_Plugin implements Typecho_Plugin_Interface
 	 * @return void
 	 */
     public static function header(){
+		if(Typecho_Widget::widget('Widget_Options')->Plugin('ImageBox')->jquery=='1'){
+			echo '<script src="//lib.sinaapp.com/js/jquery/1.9.1/jquery-1.9.1.min.js"></script>';
+		}
 	    echo '<link href="'.Helper::options()->pluginUrl.'/ImageBox/ImageBox.css" rel="stylesheet">';
     }
 	/**
